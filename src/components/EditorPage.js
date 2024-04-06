337ddaed262b5df8b78c1809aa16c899d0b1dccc
Divyanshi <agarwald264@gmail.com>
2024-04-06 15:32:16 +0530

    Editor Page left side done

--- src/components/EditorPage.js
+++ src/components/EditorPage.js
@@ -1,8 +1,37 @@
 import React from 'react'
+import { useState } from 'react'
+import Client from './Client'
 
 const EditorPage = () => {
+  const [clients,setClients]=useState([
+    {socketId:1,userName:"divyanshi"},
+    {socketId:2,userName:"rashi"},
+  ])
   return (
-    <div>EditorPage</div>
+    <div className='container-fluid vh-100'>
+      <div className='row h-100'>
+        <div className='col-md-2 bg-dark text-light d-flex flex-column h-100' style={{boxShadow:"2px 0px 4px rgba(0,0,0,0.1)"}}>
+          <img src="/codecast.png" alt="codecast" className='img-fluid mx-auto' style={{maxWidth:"150px", marginTop:"-43px"}}/>
+          <hr style={{marginTop:"-3rem"}}/>
+          <div className='d-flex flex-column overflow-auto'>
+            {clients.map((c)=>{
+              return(
+                  <Client key={c.socketId} username={c.userName}/>
+              )
+            })} 
+          </div>
+          
+          <div className='mt-auto'>
+          <hr />
+             <button className='btn btn-success'>Copy Room ID</button>
+             <button className='btn btn-danger mt-2 mb-2 px-3 btn-block'>Leave Room</button>
+          </div>
+        </div>
+        <div className='col-md-10 text-light d-flex flex-column h-100'>
+          editor
+        </div>
+      </div>
+    </div>
   )
 }
 
