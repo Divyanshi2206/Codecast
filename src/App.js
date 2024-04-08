27405d8c819f132ede8f6b69bbbf20ab120e1389
Divyanshi <agarwald264@gmail.com>
2024-04-08 11:08:12 +0530

    Random Id is generated by uuid and frontend part is completed

--- src/App.js
+++ src/App.js
@@ -3,10 +3,11 @@ import './App.css';
 import Home from './components/Home';
 import EditorPage from './components/EditorPage';
 import {Routes,Route} from "react-router-dom"
-
+import {Toaster} from "react-hot-toast"
 function App() {
   return (
     <>
+    <Toaster position='top-center'/>
      <Routes>
       <Route path="/" element={<Home/>}/>
       <Route path="/editor/:roomId" element={<EditorPage/>}/>
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,18 @@
-import React from 'react'
+import {React,useState} from 'react'
+import { toast } from 'react-hot-toast';
+import {v4 as uuid} from 'uuid'
 
 const Home = () => {
+    const [roomId,setroomId]=useState("");
+    const [userName,setuserName]=useState("")
+
+    const generateRoomId=(e)=>{
+        e.preventDefault()
+        const id=uuid();
+        setroomId(id)
+        toast.success("Room Id is generated")
+    }
+
   return (
     <div className='container-fluid'>
        <div className='row justify-content-center align-items-center min-vh-100'>
@@ -10,13 +22,13 @@ const Home = () => {
                         <img className="img-fluid mx-auto d-block" src="./codecast.png" alt="logo" style={{maxWidth:"150px"}}/>
                         <h4 className='text-light'>Enter the ROOM ID</h4>
                         <div className='form-group'>
-                            <input typ="text" className='form-control mb-2' placeholder='ROOM ID'/>
+                            <input type="text" className='form-control mb-2' placeholder='ROOM ID' value={roomId} onChange={(e)=>setroomId(e.target.value)}/>
                         </div>
                         <div className='form-group'>
-                            <input typ="text" className='form-control mb-2' placeholder='USERNAME'/>
+                            <input type="text" className='form-control mb-2' placeholder='USERNAME' value={userName} onChange={(e)=>setuserName(e.target.value)}/>
                         </div>
                         <button className='btn btn-success btn-lg btn-block'>JOIN</button>
-                        <p className='mt-3 text-light'>Don't have a room ID? create <span className='text-success p-2' style={{cursor:"pointer"}}>New Room</span></p>
+                        <p className='mt-3 text-light'>Don't have a room ID? create <span className='text-success p-2' style={{cursor:"pointer"}} onClick={generateRoomId}>New Room</span></p>
 
                     </div>
                 </div>
